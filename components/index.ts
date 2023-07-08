11f3872ca1476a573397cd4134c54fc4cba026e1
Ugur Izzet Onal <izzetonal46@gmail.com>
2023-07-08 08:59:34 +0200

    img bugs need to be fix

--- components/index.ts
+++ components/index.ts
@@ -1,6 +1,19 @@
-import Hero from '../components/Hero'
-import CustomButton from '../components/CustomButton'
+import CarCard from "./CarCard";
+import CustomButton from "./CustomButton";
+import CustomFilter from "./CustomFilter";
+import Footer from "./Footer";
+import NavBar from "./Navbar";
+import ShowMore from "./ShowMore";
+import SearchBar from "./Searchbar";
+import Hero from "./Hero";
+
 export {
-    Hero,
-    CustomButton
-}
+  Hero,
+  CarCard,
+  CustomButton,
+  CustomFilter,
+  Footer,
+  NavBar,
+  ShowMore,
+  SearchBar,
+};
